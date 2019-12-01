b7a808f93db9d808aa9ccfe831935dcada105c82
Maxim <maksfomin@gmail.com>
2019-12-01 22:26:28 +0300

    Create HW 6.1

--- src/main/java/Lesson6/Homework/Array.java
+++ src/main/java/Lesson6/Homework/Array.java
@@ -1,7 +1,39 @@
 package Lesson6.Homework;
+import java.util.Arrays;
 
 public class Array {
+
     public static void main(String[] args) {
 
+        int[] array = {1, 2, 4, 4, 2, 3, 4, 1, 7};
+        System.out.println("Входной массив: " + Arrays.toString(array));
+        replaceNums(array);
+        System.out.println("Массив после оброботки: " + Arrays.toString(replaceNums(array)));
+
+    }
+
+    public static int[] replaceNums(int[] array) {
+        int[] nums = new int[2];
+        int count = 1;
+
+        StringBuilder stringArray = new StringBuilder(Arrays.toString(array));
+        stringArray.reverse();
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 4)
+                break;
+            else if (i == array.length - 1)
+                throw new RuntimeException();
+        }
+
+        for (int i = 0; i < stringArray.length(); i++) {
+            if (stringArray.charAt(i) == '4')
+                break;
+            if ((stringArray.charAt(i) != ' ') && (stringArray.charAt(i) != ',') && (stringArray.charAt(i) != ']')) {
+                nums[count] = Integer.parseInt(String.valueOf(stringArray.charAt(i)));
+                count--;
+            }
+        }
+        return nums;
     }
 }
