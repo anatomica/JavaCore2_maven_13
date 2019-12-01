b5e261abcbae552e8d62eb498902a35ce1dda3fc
Maxim <maksfomin@gmail.com>
2019-12-01 19:01:48 +0300

    Start HW 6

---
+++ src/main/java/Lesson6/Homework/Array.java
@@ -0,0 +1,7 @@
+package Lesson6.Homework;
+
+public class Array {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/Lesson6/Homework/ArrayTest.java
@@ -0,0 +1,5 @@
+package Lesson6.Homework;
+
+public class ArrayTest {
+    
+}
