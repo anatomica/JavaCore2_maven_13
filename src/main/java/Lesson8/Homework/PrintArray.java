afa5108bcc03c2541bf3c16c09fbb85051087c3b
Maxim <maksfomin@gmail.com>
2019-12-07 21:43:43 +0300

    Create HW 1.2

---
+++ src/main/java/Lesson8/Homework/PrintArray.java
@@ -0,0 +1,14 @@
+package Lesson8.Homework;
+
+public class PrintArray {
+
+    public static void printArr(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+}
